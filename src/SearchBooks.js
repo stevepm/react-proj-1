f843c2b91ef0534e795b65f84e9d3ae974d3dd7d
Steven Magelowitz <steven.magelowitz@gmail.com>
2017-07-08 22:42:16 -0700

    go back to main page on close search

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class SearchBooks extends Component {
     render() {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                    <Link
+                        to="/"
+                        className="close-search">
+                        Close
+                    </Link>
                     <div className="search-books-input-wrapper">
                         <input type="text" placeholder="Search by title or author"/>
                     </div>
